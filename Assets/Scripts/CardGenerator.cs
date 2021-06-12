190fbd86f60311d9fdd79fef1025058735a2d76f
Jordan <jordanlevy96@gmail.com>
2021-06-12 13:29:58 +0100

    offset generated card

--- Assets/Scripts/CardGenerator.cs
+++ Assets/Scripts/CardGenerator.cs
@@ -9,6 +9,9 @@ public class CardGenerator : MonoBehaviour
     public Tilemap Grid;
     private System.Random Rand = new System.Random();
 
+    private static Vector3 CARD_SCALE = new Vector3(1.3f, 1.3f, 1);
+    private static Vector3 CARD_OFFSET = new Vector3(8, 0, 0);
+
     public GameObject DrawCard()
     {
         Debug.Log("Drawing Card...");
@@ -17,7 +20,8 @@ public class CardGenerator : MonoBehaviour
 
         GameObject card = new GameObject();
         card.transform.SetParent(Grid.transform);
-        card.transform.localScale = new Vector3(1.3f, 1.3f, 1);
+        card.transform.localScale = CARD_SCALE;
+        card.transform.Translate(CARD_OFFSET);
         SpriteRenderer sr = card.AddComponent<SpriteRenderer>();
         sr.sprite = tile.sprite;
         card.AddComponent<BoxCollider2D>();
