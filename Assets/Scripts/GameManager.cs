41d01f58ad48353983d140b4cb27fe044663985e
Jordan <jordanlevy96@gmail.com>
2021-06-12 16:22:26 +0100

    turn off datingcourt with e

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static List<GameObject> Hand;
+    private static GameObject DatingCourt;
 
     // Start is called before the first frame update
     void Start()
     {
         Hand = new List<GameObject>(); 
+        DatingCourt = GameObject.Find("DatingCourt");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public static void HideDating()
+    {
+        DatingCourt.SetActive(false);
     }
 }
