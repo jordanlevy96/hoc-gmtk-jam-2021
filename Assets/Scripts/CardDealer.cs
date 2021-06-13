f6c5277aaf699fc4d390c585abe5769f35b263ea
Zak Thompson <zakthompson@fastmail.com>
2021-06-13 12:30:43 -0400

    Add deal sound

--- Assets/Scripts/CardDealer.cs
+++ Assets/Scripts/CardDealer.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class CardDealer : MonoBehaviour
 {
     public CardGenerator Generator;
+    private AudioSource dealCardSound;
 
     // Update is called once per frame
+    public void Start() {
+        dealCardSound = this.GetComponent<AudioSource>();
+    }
+    
     void Update()
     {
         if (GameManager.Hand.Count == 0)
         {
             GameManager.Hand.Add(Generator.DrawCard());
+            dealCardSound.Play();
         }
     }
 }
